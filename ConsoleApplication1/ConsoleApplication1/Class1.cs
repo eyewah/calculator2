e0f6c001d3e091faea7c194c5076cfe00a402d1e
eyewah <vladis5422@gmail.com>
2022-10-04 09:47:03 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Calculator
+    {
+        public static string ProcessStatement(string statement)
+        {
+            string opearandA = statement.Substring(0, 1);
+            string opearandB = statement.Substring(2, 1);
+            string operation = statement.Substring(1, 1);
+            int a = int.Parse(opearandA);
+            int b = int.Parse(opearandB);
+            string result = "";
+            switch (operation)
+            {
+                case "+":
+                    result = (a+b).ToString();
+                    break;
+                case "-":
+                    result = (a-b).ToString();
+                    break;
+                case "/":
+                    result = Divide(opearandA, opearandB);
+                    break;
+                case "*":
+                    result = (a*b).ToString();
+                    break;
+                default:
+
+                    break;
+            }
+            string resultString = result.ToString();
+            return resultString;
+        }
+
+        public static string Divide(string a, string b)
+        {
+            string result = "";
+            if (b == "0")
+            {
+                result = "На ноль делить нельзя";
+            }
+            else
+            {
+                int opearandA = int.Parse(a);
+                int opearandB = int.Parse(b);
+                result = (opearandA / opearandB).ToString();
+            }
+            return result;
+        }
+        
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testResult = Calculator.ProcessStatement("1+1");
+            Console.WriteLine("результат сложения   " + testResult);
+            testResult = Calculator.ProcessStatement("2*3");
+            Console.WriteLine("результат умножения   " + testResult);
+            testResult = Calculator.ProcessStatement("3/3");
+            Console.WriteLine("результат деления   " + testResult);
+            testResult = Calculator.ProcessStatement("6-3");
+            Console.WriteLine("результат вычитания   " + testResult);
+            testResult = Calculator.ProcessStatement("6/0");
+            Console.WriteLine("результат деления   " + testResult);
+
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
